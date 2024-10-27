b80bb0c6fe57585912eb62fbc437844dbec56173
bhsaio145 <brianhsiao2001@gmail.com>
2024-10-27 18:38:52 -0400

    Added Temp Win and Lose conditions

--- resources/global/events.gd
+++ resources/global/events.gd
@@ -11,6 +11,7 @@ signal card_played(card: Card)
 signal player_hand_drawn
 signal player_hand_discarded
 signal player_turn_ended
+signal player_died
 
 # Enemy-related events
 signal enemy_action_taken(enemy: Enemy)
